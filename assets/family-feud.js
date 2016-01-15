453fc226f42ec59f5ab59b4888916f1eb4edeefc
Jonathan Kuo <jkuo@learninghouse.com>
2016-01-15 04:00:34 -0500

    Working prototype

--- assets/family-feud.js
+++ assets/family-feud.js
@@ -9,30 +9,42 @@ var TeamModel = Backbone.Model.extend({
 		this.set("score", this.get("score") + points);
 	},
 	isActive: function () {
-		this.get("active");
+		return this.get("active");
 	},
 	addStrike: function () {
 		this.set("strikes", this.get("strikes") + 1);
+		$(".strikes").show();
+		var strikes = this.get("strikes");
+		for(i = 1; i <= strikes; i++){
+			$("#strike-"+i).show();
+		}
+		setTimeout(function(){
+			for(i = 1; i <= strikes; i++){
+				$("#strike-"+i).hide();
+				$(".strikes").hide();
+			}
+		}, 1500);
 		if(this.get("strikes") == 3){
 			this.trigger("strikeout");
 			this.set("strikes", 0);
 		}
 	},
 	isStealing: function() {
-		this.get("steal");
+		return this.get("steal");
 	}
 });
 
 var TeamView = Backbone.View.extend({
 	initialize: function() {
-		this.model.on("change:score", this.render);
+		this.model.on("change:score", this.render, this);
 	},
 	tagName: "div",
 	boardSide: "left",
-	template: _.template('<div id="team-<%= id %>"><div class="team-score"><%= score %></div><div class="team-name" style="background-color:<%= color %>"><%= name %></div></div>'),
+	template: _.template('<div class="team" id="team-<%= id %>"><div class="team-score"><%= score %></div><div class="team-name" style="background-color:<%= color %>"><%= name %></div></div>'),
+	innerTemplate: _.template('<div class="team-score"><%= score %></div><div class="team-name" style="background-color:<%= color %>"><%= name %></div>'),
 	render: function() {
-		if(this.$el){
-			this.$el.html(this.template(this.model.attributes));
+		if($("#team-"+this.model.id).length){
+			$("#team-"+this.model.id).html(this.innerTemplate(this.model.attributes));
 		}else{
 			$(".team-scores."+this.boardSide).append(this.template(this.model.attributes));
 		}
@@ -46,33 +58,36 @@ var SurveyView = Backbone.View.extend({
 	activeSurveyId: null,
 	flippedAnswers: 0,
 	pickSurvey: function(survey_id) {
-		if(!survey_id){
-			if(!this.activeSurveyId){
-				this.activeSurveyId = 1;
-			}else{
+		if(survey_id == undefined){
+			if(this.activeSurveyId == null){
+				this.activeSurveyId = 0;
+			}else if(this.activeSurveyId < this.surveys.length - 1){
 				this.activeSurveyId++;
+			}else{
+				this.activeSurveyId = 0;
 			}
 		}else{
 			this.activeSurveyId = survey_id;
 		}
 	},
-	questionTemplate: _.template('<h3 class="survey-question"><%= question =></h3>'),
+	questionTemplate: _.template('<h3 class="survey-question"><%= question %></h3>'),
 	answerTemplate: _.template('<%= answer %> - <%= points %>'),
 	render: function(){
 		var activeSurvey = this.surveys[this.activeSurveyId];
-		this.$(".answer").flip(false);
-		this.$(".answer").once("click", this.flipAnswer, this);
-		this.$(".survey").html(this.questionTemplate(activeSurvey.question));
-		_.each(activeSurvey.answers, this.renderAnswer());
-		this.$(".score").html("0");
+		$(".answer").flip(false);
+		$(".answer").one("click", $.proxy(this.flipAnswer, this));
+		$(".survey").html(this.questionTemplate({question: activeSurvey.question}));
+		_.each(activeSurvey.answers, this.renderAnswer, this);
+		$(".score").html("0");
 	},
 	renderAnswer: function(answer, rank) {
-		this.$("#answer-"+rank+" .back").html(this.answerTemplate(answer));
+		$("#answer-"+(rank+1)+" .back").html(this.answerTemplate(answer));
 	},
 	flipAnswer: function(e) {
-		e.target.flip(true);
+		$(e.currentTarget).flip(true);
 		var activeSurvey = this.surveys[this.activeSurveyId];
-		this.updateScore(activeSurvey.answers[e.target.id.substr(-1)].points);
+		var activeAnswer = e.currentTarget.id.substr(-1) - 1;
+		this.updateScore(activeSurvey.answers[activeAnswer].points);
 		this.flippedAnswers++;
 		if(this.flippedAnswers == activeSurvey.answers.length){
 			this.trigger("complete", this.score);
@@ -80,7 +95,13 @@ var SurveyView = Backbone.View.extend({
 	},
 	updateScore: function(points) {
 		this.score = this.score + points;
-		this.$(".score").html(this.score);
+		$(".score").html(this.score);
+	},
+	resetScore: function() {
+		this.score = 0;
+		this.flippedAnswers = 0;
+		$(".answer").off("click", $.proxy(this.flipAnswer, this));
+		$(".score").html(this.score);
 	}
 });
 
@@ -92,13 +113,13 @@ $(document).ready(function (){
 	}else{
 		var team_half = game_data.tn.length/2;
 	}
-	for(i = 0; i <= game_data.tn.length; i++){
+	for(i = 0; i < game_data.tn.length; i++){
 		var tm = new TeamModel({
 			id: i + 1,
 			name: game_data.tn[i],
 			color: game_data.tc[i]
 		});
-		if(i <= team_half){
+		if(i + 1 <= team_half){
 			var board_side = "left";
 		}else {
 			var board_side = "right";
@@ -107,24 +128,30 @@ $(document).ready(function (){
 		teamid = teamid.toString();
 		teams[i] = new TeamView({
 			id: "team-"+teamid,
-			model: tm,
-			boardSide: board_side
+			model: tm
 		});
+		teams[i].boardSide = board_side;
 		teams[i].render();
 	}
 	_.each(teams, function(team, team_id) {
 		team.model.on("strikeout", function(){
 			team.model.set("active", false);
-			teams[team_id+1].model.set("active", true);
-			teams[team_id+1].model.set("steal", true);
+			if(team_id+1 < teams.length){
+				teams[team_id+1].model.set("active", true);
+				teams[team_id+1].model.set("steal", true);
+			}else{
+				teams[0].model.set("active", true);
+				teams[0].model.set("steal", true);
+			}
 		});
 	});
 	teams[0].model.set("active", true);
 	
 	$(".survey-wrapper .answer").flip({trigger: "manual", axis: "x"});
 	
-	var survey = new SurveyView({surveys: survey_data});
-	survey.pickSurvey();
+	var survey = new SurveyView();
+	survey.surveys = survey_data;
+	survey.pickSurvey(0);
 	survey.render();
 	
 	survey.on("complete", function(score){
@@ -145,11 +172,12 @@ $(document).ready(function (){
 			active_team.model.set("steal", false);
 		}
 		teams[team_view_id] = active_team;
+		survey.resetScore();
 		survey.pickSurvey();
-		survey.render();
+		setTimeout($.proxy(survey.render, survey), 3000);
 	});
 	
-	survey.$(".answer").on("click", function(){
+	$(".answer").on("click", function(){
 		var active_team = _.find(teams, function(team) {
 			return team.model.isActive();
 		});
@@ -164,6 +192,12 @@ $(document).ready(function (){
 				return team.model.isActive();
 			});
 			if(active_team.model.isStealing()){
+				$(".strikes").show();
+				$("#strike-1").show();
+				setTimeout(function(){
+					$("#strike-1").hide();
+					$(".strikes").hide();
+				}, 1500);
 				active_team.model.set("steal", false);
 				active_team.model.set("active", false);
 				var team_view_id = active_team.model.id - 1;
@@ -172,11 +206,11 @@ $(document).ready(function (){
 				}else {
 					teams[team_view_id - 1].model.set("active", true);
 				}
+				survey.trigger("complete", survey.score);
 			}else{
 				active_team.model.addStrike();
 			}
 			teams[team_view_id] = active_team;
-			survey.trigger("complete", survey.score);
 		} 
 	});
 });
